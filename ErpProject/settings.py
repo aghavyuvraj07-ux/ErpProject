187b42c1b9376cc868a7130e437cda917d0639b9
Yuvraj Aghav <aghavyuvraj@gmail.com>
2026-09-23 23:10:12 +0530

    Configure database using environment variables

--- ErpProject/settings.py
+++ ErpProject/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
 
     'LeadService',
     'StudentService',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,7 +120,6 @@ USE_TZ = True
 
 
 # Static files
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
 
@@ -126,3 +127,10 @@ STATIC_URL = 'static/'
 # Default primary key field type
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# CORS settings for Angular
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+]
